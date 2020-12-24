cf605850020d7101503cee00496538388ecab43e
ifui <ifui@foxmail.com>
2020-12-24 17:17:55 +0800

    created: Modules/Knowledge 模块

---
+++ Modules/Knowledge/Providers/KnowledgeServiceProvider.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace Modules\Knowledge\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Eloquent\Factory;
+
+class KnowledgeServiceProvider extends ServiceProvider
+{
+    /**
+     * Boot the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->registerTranslations();
+        $this->registerConfig();
+        $this->registerViews();
+        $this->registerFactories();
+        $this->loadMigrationsFrom(module_path('Knowledge', 'Database/Migrations'));
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->register(RouteServiceProvider::class);
+    }
+
+    /**
+     * Register config.
+     *
+     * @return void
+     */
+    protected function registerConfig()
+    {
+        $this->publishes([
+            module_path('Knowledge', 'Config/config.php') => config_path('knowledge.php'),
+        ], 'config');
+        $this->mergeConfigFrom(
+            module_path('Knowledge', 'Config/config.php'), 'knowledge'
+        );
+    }
+
+    /**
+     * Register views.
+     *
+     * @return void
+     */
+    public function registerViews()
+    {
+        $viewPath = resource_path('views/modules/knowledge');
+
+        $sourcePath = module_path('Knowledge', 'Resources/views');
+
+        $this->publishes([
+            $sourcePath => $viewPath
+        ],'views');
+
+        $this->loadViewsFrom(array_merge(array_map(function ($path) {
+            return $path . '/modules/knowledge';
+        }, \Config::get('view.paths')), [$sourcePath]), 'knowledge');
+    }
+
+    /**
+     * Register translations.
+     *
+     * @return void
+     */
+    public function registerTranslations()
+    {
+        $langPath = resource_path('lang/modules/knowledge');
+
+        if (is_dir($langPath)) {
+            $this->loadTranslationsFrom($langPath, 'knowledge');
+        } else {
+            $this->loadTranslationsFrom(module_path('Knowledge', 'Resources/lang'), 'knowledge');
+        }
+    }
+
+    /**
+     * Register an additional directory of factories.
+     *
+     * @return void
+     */
+    public function registerFactories()
+    {
+        if (! app()->environment('production') && $this->app->runningInConsole()) {
+            app(Factory::class)->load(module_path('Knowledge', 'Database/factories'));
+        }
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [];
+    }
+}
